18bcfdbe227bdab7a4998ef074c6da8e71ca10d3
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-07 07:53:09 +0200

    Handled the input of user guess and checkign if there is any match

--- hangman-game.py
+++ hangman-game.py
@@ -12,3 +12,13 @@ for letter in random_word:
     display += "_"
 
 print(display)
+
+while not game_over:
+    guessed_letter = input ("guess a letter").lower()
+
+    for position in range(len(random_word)):
+        letter = random_word[position]
+
+        if letter == guessed_letter:
+            display[position] = guessed_letter
+    print(display)
