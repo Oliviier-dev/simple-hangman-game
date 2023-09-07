c12d6fa6e552f84ad4f54a6da2f1c5912ce96d6e
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-07 07:51:16 +0200

    Handled the display of dashes for the random word

--- hangman-game.py
+++ hangman-game.py
@@ -4,3 +4,11 @@ word_list = ["little", "book", "trouble", "apple"]
 random_word = random.choice(word_list)
 Lives = 6
 display = []
+
+game_over = False
+print(random_word)
+
+for letter in random_word:
+    display += "_"
+
+print(display)
